9d1f677c4d98c06c5e30d969baf3d363fcdba5af
Alexhqolma <66127994+Alexhqolma@users.noreply.github.com>
2022-09-26 12:45:01 +0300

    DB connection fix

--- src/main/java/taxi/service/util/ConnectionUtil.java
+++ src/main/java/taxi/service/util/ConnectionUtil.java
@@ -6,11 +6,10 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL =
-            "jdbc:mysql://localhost:3306/taxi?useUnicode=true&serverTimezone=UTC";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "12345";
-    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "URL";
+    private static final String USERNAME = "Login";
+    private static final String PASSWORD = "Password";
+    private static final String JDBC_DRIVER = "Driver";
 
     static {
         try {
